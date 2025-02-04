b5fc29a880292973c44f9275209764c1aefd9064
pitang <wpeffect@gmail.com>
2025-02-05 02:25:30 +0900

    ✅ formatDate 테스트 코드 작성

--- src/__tests__/unit/easy.dateUtils.spec.ts
+++ src/__tests__/unit/easy.dateUtils.spec.ts
@@ -276,11 +276,23 @@ describe('fillZero', () => {
 });
 
 describe('formatDate', () => {
-  it('날짜를 YYYY-MM-DD 형식으로 포맷팅한다', () => {});
+  it('날짜를 YYYY-MM-DD 형식으로 포맷팅한다', () => {
+    const date = new Date('2025-02-03');
+    expect(formatDate(date)).toBe('2025-02-03');
+  });
 
-  it('day 파라미터가 제공되면 해당 일자로 포맷팅한다', () => {});
+  it('day 파라미터가 제공되면 해당 일자로 포맷팅한다', () => {
+    const date = new Date('2025-02-03');
+    expect(formatDate(date, 18)).toBe('2025-02-18');
+  });
 
-  it('월이 한 자리 수일 때 앞에 0을 붙여 포맷팅한다', () => {});
+  it('월이 한 자리 수일 때 앞에 0을 붙여 포맷팅한다', () => {
+    const date = new Date('2025-2-23');
+    expect(formatDate(date)).toBe('2025-02-23');
+  });
 
-  it('일이 한 자리 수일 때 앞에 0을 붙여 포맷팅한다', () => {});
+  it('일이 한 자리 수일 때 앞에 0을 붙여 포맷팅한다', () => {
+    const date = new Date('2025-02-3');
+    expect(formatDate(date)).toBe('2025-02-03');
+  });
 });
