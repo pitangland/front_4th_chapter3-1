8bdce66141762412c04e945add8164b5bcc8f995
pitang <wpeffect@gmail.com>
2025-02-05 02:05:10 +0900

    ✅ formatWeek 테스트 코드 작성

--- src/__tests__/unit/easy.dateUtils.spec.ts
+++ src/__tests__/unit/easy.dateUtils.spec.ts
@@ -152,17 +152,41 @@ describe('getEventsForDay', () => {
 });
 
 describe('formatWeek', () => {
-  it('월의 중간 날짜에 대해 올바른 주 정보를 반환한다', () => {});
+  it('월의 중간 날짜에 대해 올바른 주 정보를 반환한다', () => {
+    const date = new Date('2025-02-17');
+    const result = formatWeek(date);
+    expect(result).toBe('2025년 2월 3주');
+  });
 
-  it('월의 첫 주에 대해 올바른 주 정보를 반환한다', () => {});
+  it('월의 첫 주에 대해 올바른 주 정보를 반환한다', () => {
+    const date = new Date('2025-02-03');
+    const result = formatWeek(date);
+    expect(result).toBe('2025년 2월 1주');
+  });
 
-  it('월의 마지막 주에 대해 올바른 주 정보를 반환한다', () => {});
+  it('월의 마지막 주에 대해 올바른 주 정보를 반환한다', () => {
+    const date = new Date('2025-02-28');
+    const result = formatWeek(date);
+    expect(result).toBe('2025년 2월 4주');
+  });
 
-  it('연도가 바뀌는 주에 대해 올바른 주 정보를 반환한다', () => {});
+  it('연도가 바뀌는 주에 대해 올바른 주 정보를 반환한다', () => {
+    const date = new Date('2025-01-01');
+    const result = formatWeek(date);
+    expect(result).toBe('2025년 1월 1주');
+  });
 
-  it('윤년 2월의 마지막 주에 대해 올바른 주 정보를 반환한다', () => {});
+  it('윤년 2월의 마지막 주에 대해 올바른 주 정보를 반환한다', () => {
+    const date = new Date('2024-02-29');
+    const result = formatWeek(date);
+    expect(result).toBe('2024년 2월 5주');
+  });
 
-  it('평년 2월의 마지막 주에 대해 올바른 주 정보를 반환한다', () => {});
+  it('평년 2월의 마지막 주에 대해 올바른 주 정보를 반환한다', () => {
+    const date = new Date('2025-02-28');
+    const result = formatWeek(date);
+    expect(result).toBe('2025년 2월 4주');
+  });
 });
 
 describe('formatMonth', () => {
