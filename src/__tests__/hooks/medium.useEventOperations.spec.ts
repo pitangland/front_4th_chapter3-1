f46705f556d4837b29b286c20a229ce78f53f687
pitang <wpeffect@gmail.com>
2025-02-06 02:51:08 +0900

    ✅ useEventOperation 테스트 코드 작성

--- src/__tests__/hooks/medium.useEventOperations.spec.ts
+++ src/__tests__/hooks/medium.useEventOperations.spec.ts
@@ -21,16 +21,141 @@ vi.mock('@chakra-ui/react', async () => {
   };
 });
 
-it('저장되어있는 초기 이벤트 데이터를 적절하게 불러온다', async () => {});
+describe('useEventOperations', () => {
+  it('저장되어있는 초기 이벤트 데이터를 적절하게 불러온다', async () => {
+    setupMockHandlerCreation(); // 이벤트 생성 Mock API 설정
+    const { result } = renderHook(() => useEventOperations(false));
+    await act(async () => {
+      await result.current.fetchEvents();
+    });
+    expect(toastFn).toHaveBeenCalledWith({
+      title: '일정 로딩 완료!',
+      status: 'info',
+      duration: 1000,
+    });
+  });
 
-it('정의된 이벤트 정보를 기준으로 적절하게 저장이 된다', async () => {});
+  it('정의된 이벤트 정보를 기준으로 적절하게 저장이 된다', async () => {
+    setupMockHandlerCreation(); // 이벤트 생성 Mock API 설정
+    const { result } = renderHook(() => useEventOperations(false));
+    const newEvent: Event = {
+      id: '1',
+      title: '회의',
+      date: '2025-02-01',
+      startTime: '10:00',
+      endTime: '18:00',
+      description: '팀 회의',
+      location: '회의실 A',
+      category: '업무',
+      repeat: { type: 'none', interval: 1 },
+      notificationTime: 10,
+    };
+    await act(async () => {
+      await result.current.saveEvent(newEvent);
+    });
+    expect(toastFn).toHaveBeenCalledWith({
+      title: '일정이 추가되었습니다.',
+      status: 'success',
+      duration: 3000,
+      isClosable: true,
+    });
+  });
 
-it("새로 정의된 'title', 'endTime' 기준으로 적절하게 일정이 업데이트 된다", async () => {});
+  it("새로 정의된 'title', 'endTime' 기준으로 적절하게 일정이 업데이트 된다", async () => {
+    setupMockHandlerUpdating(); // 이벤트 수정 Mock API 설정
+    const { result } = renderHook(() => useEventOperations(true));
+    const updatedEvent: Event = {
+      id: '1',
+      title: '업데이트된 회의',
+      date: '2024-10-15',
+      startTime: '09:00',
+      endTime: '15:00',
+      description: '업데이트된 팀 미팅',
+      location: '회의실 B',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 10,
+    };
 
-it('존재하는 이벤트 삭제 시 에러없이 아이템이 삭제된다.', async () => {});
+    await act(async () => {
+      await result.current.saveEvent(updatedEvent);
+    });
+    expect(toastFn).toHaveBeenCalledWith({
+      title: '일정이 수정되었습니다.',
+      status: 'success',
+      duration: 3000,
+      isClosable: true,
+    });
+  });
 
-it("이벤트 로딩 실패 시 '이벤트 로딩 실패'라는 텍스트와 함께 에러 토스트가 표시되어야 한다", async () => {});
+  it('존재하는 이벤트 삭제 시 에러없이 아이템이 삭제된다.', async () => {
+    setupMockHandlerDeletion(); // 이벤트 삭제 Mock API 설정
+    const { result } = renderHook(() => useEventOperations(false));
 
-it("존재하지 않는 이벤트 수정 시 '일정 저장 실패'라는 토스트가 노출되며 에러 처리가 되어야 한다", async () => {});
+    await act(async () => {
+      await result.current.deleteEvent('1');
+    });
+    expect(toastFn).toHaveBeenCalledWith({
+      title: '일정이 삭제되었습니다.',
+      status: 'info',
+      duration: 3000,
+      isClosable: true,
+    });
+  });
 
-it("네트워크 오류 시 '일정 삭제 실패'라는 텍스트가 노출되며 이벤트 삭제가 실패해야 한다", async () => {});
+  it("이벤트 로딩 실패 시 '이벤트 로딩 실패'라는 텍스트와 함께 에러 토스트가 표시되어야 한다", async () => {
+    server.use(http.get('/api/events', () => HttpResponse.error()));
+    const { result } = renderHook(() => useEventOperations(false));
+
+    await act(async () => {
+      await result.current.fetchEvents();
+    });
+    expect(toastFn).toHaveBeenCalledWith({
+      title: '이벤트 로딩 실패',
+      status: 'error',
+      duration: 3000,
+      isClosable: true,
+    });
+  });
+
+  it("존재하지 않는 이벤트 수정 시 '일정 저장 실패'라는 토스트가 노출되며 에러 처리가 되어야 한다", async () => {
+    server.use(http.put('/api/events/:id', () => HttpResponse.error()));
+    const { result } = renderHook(() => useEventOperations(true));
+    const nonExitstingEvent: Event = {
+      id: '99',
+      title: '삭제할 이벤트',
+      date: '2024-10-15',
+      startTime: '09:00',
+      endTime: '10:00',
+      description: '삭제할 이벤트입니다',
+      location: '어딘가',
+      category: '기타',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 10,
+    };
+    await act(async () => {
+      await result.current.saveEvent(nonExitstingEvent);
+    });
+    expect(toastFn).toHaveBeenCalledWith({
+      title: '일정 저장 실패',
+      status: 'error',
+      duration: 3000,
+      isClosable: true,
+    });
+  });
+
+  it("네트워크 오류 시 '일정 삭제 실패'라는 텍스트가 노출되며 이벤트 삭제가 실패해야 한다", async () => {
+    server.use(http.delete('/api/events/:id', () => HttpResponse.error()));
+    const { result } = renderHook(() => useEventOperations(false));
+
+    await act(async () => {
+      await result.current.deleteEvent('99');
+    });
+    expect(toastFn).toHaveBeenCalledWith({
+      title: '일정 삭제 실패',
+      status: 'error',
+      duration: 3000,
+      isClosable: true,
+    });
+  });
+});
